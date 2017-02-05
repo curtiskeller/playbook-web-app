56c994695b02a85d4ac2ea343d3f7ea9ebc0cfa2
Corey Keller <cakeller43@gmail.com>
2017-02-05 12:31:06 -0600

    fix duplicate response bug

--- playbook_api/web/controllers/playbooklibrary_controller.ex
+++ playbook_api/web/controllers/playbooklibrary_controller.ex
@@ -2,12 +2,9 @@ defmodule PlaybookApi.PlaybookLibraryController do
 
   use PlaybookApi.Web, :controller
 
-  plug :action
-
   def index(conn, _params) do
     res = Enum.map(1..20, fn(x) -> %{:id => x,:name => "Playbook", :owner => "Corey"} end)
 
-    #res = [%{ :id => 1, :name => "Playbook1", :owner => "Corey"}, %{ :id => 2, :name => "Golden Deagles", :owner => "Curtis"}]
     put_status(conn,200)
     json(conn,res)
   end
