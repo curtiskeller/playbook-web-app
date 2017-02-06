ec9c62fab1dd06ecbfde12d3b5b21292d0d374f6
Corey Keller <cakeller43@gmail.com>
2017-02-06 08:07:25 -0600

    Add random room generator for scratch

--- playbook_website/src/components/layout/TopNav.jsx
+++ playbook_website/src/components/layout/TopNav.jsx
@@ -2,8 +2,19 @@ import React, { Component } from 'react';
 import { Link } from 'react-router'
 import { IndexLink } from 'react-router'
 
+
+
+
 class TopNav extends Component {
+
+    getRandomId() {
+      var min = 1;
+      var max = 999999;
+      return Math.floor(Math.random() * (max - min)) + min;
+    }
+
     render() {
+        const scratchId = this.getRandomId();
         return (
             <nav className="navbar navbar-toggleable-md navbar-inverse bg-inverse fixed-top">
                 <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -19,7 +30,7 @@ class TopNav extends Component {
                             </IndexLink>
                         </li>
                         <li className="nav-item">
-                            <Link activeStyle={{ color: '#53acff' }} className="nav-link" to='/scratch'>
+                            <Link activeStyle={{ color: '#53acff' }} className="nav-link" to={'/scratch/' + scratchId}>
                                 <span>Scratch</span>
                             </Link>
                         </li>
