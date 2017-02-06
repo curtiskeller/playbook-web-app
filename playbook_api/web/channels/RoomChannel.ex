cf375e090830951c846c7f5c1afaa2357176a5a1
Corey Keller <cakeller43@gmail.com>
2017-02-05 20:05:52 -0600

    Cleanup

--- playbook_api/web/channels/RoomChannel.ex
+++ playbook_api/web/channels/RoomChannel.ex
@@ -5,8 +5,8 @@ defmodule PlaybookApi.RoomChannel do
 		{:ok, socket}
 	end
 
-	def join("room:" <> _private_room_id, _params, _socket) do
-    	{:error, %{reason: "unauthorized"}}
+	def join("room:" <> _room_id, _params, socket) do
+        {:ok, socket}
     end
 
     def handle_in("new_msg", %{"body" => body}, socket) do
--- playbook_website/src/components/scratch/Scratch.jsx
+++ playbook_website/src/components/scratch/Scratch.jsx
@@ -5,9 +5,7 @@ import socket from '../../channels/socket.js';
 
 class Scratch extends Component {
     render() {
-        console.log(this.props.params);
         const { scratchId } = this.props.params;
-        console.log(scratchId);
         return (
             <div>
                 <p>Scratch</p>
