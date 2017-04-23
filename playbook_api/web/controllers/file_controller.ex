c5511a6b5b28d09783f80a8a965c6d08569652d4
Corey Keller <cakeller43@gmail.com>
2017-04-23 15:48:53 -0500

    change file path. return 201

--- playbook_api/web/controllers/file_controller.ex
+++ playbook_api/web/controllers/file_controller.ex
@@ -3,11 +3,12 @@ defmodule PlaybookApi.FileController do
   use PlaybookApi.Web, :controller
 
   def create(conn, params = %{"file" => file}) do
-      #extension = Path.extname(params.filename)
-      #IO.inspect(params)
       #TODO use some unique id per user or playbook to save files.
-      #File.cp(params.path, "/media/#{extension}")
-      json(conn,"")
+      File.cp!(file.path, "C:\\media\\#{file.filename}")
+
+      conn
+      |> put_status(:created)
+      |> json(file)
   end
 
 end
